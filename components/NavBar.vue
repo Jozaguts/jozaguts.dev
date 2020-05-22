4ad27f5309b97da77c28493418ab6c90537253d4
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-05-22 16:52:27 -0500

    add navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar class="bg-main-color" app>
+    <v-app-bar-nav-icon @click="changeDrawerStatus" dark></v-app-bar-nav-icon>
+  </v-app-bar>
+</template>
+
+<script>
+import {mapMutations} from 'vuex';
+export default {
+  data() {
+    return {};
+  },
+  methods:{
+   ...mapMutations({
+     changeDrawerStatus: 'NavBar/toggleDrawerStatus'
+   }) 
+  }
+};
+</script>
+
+<style>
+</style>
