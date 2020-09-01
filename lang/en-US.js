380e0c892d19a3bc1558ffa12d28f959bf01f274
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-08-31 19:09:44 -0500

    course info added

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,9 +1,25 @@
 export default {
   titleJob: 'web Developer',
+  degree: 'Degree of IT Engineer',
   aboutMe: 'about me',
-  aboutMeContent: 'Hi I\'m Sagit I\'m from México and I\'m a Web developer with four years of experience and I have been worked with vueJs Laravel and related libraries and currently I\'m working as freelance mode I wish to have an international experience and also provide a better life for myself and my family also I aspiring to be  full-stack developer, with rich experience in developing websites. I would like to offer you a job specialized in you and your needs, Communication and specifications are the keys to a good job Thank you in advance for your time and consideration. I look forward to working with you soon.',
+  aboutMeContent: 'Hi I\'m Sagit I\'m from México and I\'m a Web developer with four years of experience and I have ' +
+    'been worked with vueJs Laravel and related libraries and currently I\'m working as freelance mode' +
+    ' my medium-term goal it\'s to have an international experience and also provide a better life for myself and my family' +
+    ' also I aspiring to be  full-stack developer, with rich experience in developing websites.' +
+    ' I would like to offer you a job specialized in you and your needs, Communication and specifications are the keys' +
+    ' to a good job Thank you in advance for your time and consideration.' +
+    ' I look forward to working with you soon.',
   introduce: 'Hi My name is Sagit!, I\'m an IT engineer focused on web development Laravel and VueJs my favorite frameworks, over the road I have been working with the most demanded libraries, languages and frameworks building and deploying technology solutions',
   callTo: 'CV',
   titleProjectContainer: 'Last projects',
-  axovia: "Here were my beginnings when I was still studying at the university I joined the company as an intern, I learned the world of back-end and front-end, with layout tasks, scripting, I began to learn laravel and a few months later I began with vuejs"
+  axovia: "Here were my beginnings when I was still studying at the university " +
+    "I joined the company as an intern, I learned the world of back-end and front-end, " +
+    "with layout tasks, scripting, I began to learn laravel and a few months later I began with vuejs",
+  university: {
+    name: 'Technological University of Bahía de Banderas - UTBB',
+    date: '2016 - 2020'
+  },
+  courses:'Course | Certifications',
+  workHistory: 'Work History',
+  academicBackground: 'Academic background'
 }
--- lang/es-MX.js
+++ lang/es-MX.js
@@ -1,9 +1,19 @@
 export default {
   titleJob: 'Desarrollador Web',
+  degree: 'Ingeniería en Tics',
   aboutMe: 'Sobre mi',
   aboutMeContent: "Hola soy Sagit soy de México y soy un desarrollador web con cuatro años de experiencia y he trabajado con vueJs Laravel y librerías relacionadas y actualmente estoy trabajando como freelance deseo tener una experiencia internacional y también proporcionar una mejor vida para mí y mi familia también aspiro a ser desarrollador de pila completa, con una rica experiencia en el desarrollo de sitios web.  Me gustaría ofrecerte un trabajo especializado en ti y tus necesidades, la comunicación y las especificaciones son las claves de un buen trabajo Gracias de antemano por tu tiempo y consideración. Espero trabajar con usted pronto.",
   introduce: 'Hola mi nombre es Sagit, soy ingeniero en tecnologías de la información enfocado en desarrollo web, Laravel y VueJs mis frameworks predilectos a lo largo del camino he trabajado con las librerías, lenguajes y frameworks más demandados creando y dando soluciones a todo tipo de proyectos, tomando cada uno de ellos de manera profesional para asi poder ofrecer la mejor experiencia de usuario al cliente y/o al usuario final.',
   callTo: 'CV',
   titleProjectContainer: 'Últimos proyectos',
-  axovia: "Aquí fueron mis inicios cuando seguía estudiando la universidad entre a la empresa en modo pasante, aprendí el mundo del backend y fronted, con tareas de maquetado, scripting,  comencé a aprende laravel y meses después comencé con vuejs"
+  axovia: "Aquí fueron mis inicios cuando seguía estudiando la universidad entre a la empresa en" +
+    " modo pasante, aprendí el mundo del backend y fronted, con tareas de maquetado, scripting," +
+    "  comencé a aprende laravel y meses después comencé con vuejs",
+  university: {
+    name: 'Universidad Tecnológica de Bahía de Banderas - UTBB',
+    date: '2016 - 2020'
+  },
+  courses:'Cursos | Certificaciones',
+  workHistory: 'Historial de trabajo',
+  academicBackground: 'Antecedentes Academicos'
 }
