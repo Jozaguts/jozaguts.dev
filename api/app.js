6855d5aba059b9dfb552b955d267302277a87d6e
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-11-02 18:55:34 -0600

    <client-only> pdf added
    Signed-off-by: Sagit Gutiérrez <jozaguts@hotmail.com>

--- api/app.js
+++ api/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+
 app.use(express.json())
 app.use(express.urlencoded({extend: true}))
 app.use(cors());
 
-app.use( require('./routes'));
+app.use(require('./Routes/index.js'));
 
 module.exports = {
   path: '/api',
