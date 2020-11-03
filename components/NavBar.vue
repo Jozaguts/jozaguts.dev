6abc557fe2bbbcc3970406dd8544258f12a63f1d
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-11-03 16:42:17 -0600

    dinamic links, to change language page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,15 +1,30 @@
 <template>
   <v-app-bar dense elevate-on-scroll height="80" app>
-    <router-link class="d-flex align-center" to="/">
-      <img src="~/assets/img/logoJz.svg" alt="logo" width="50" class="mr-2">
+    <router-link class="d-flex align-center" :to="{ path: homeRoute }">
+      <img src="~/assets/img/logoJz.svg" alt="logo" width="50" class="mr-2" />
       <h1 class="d-none d-md-flex d-lg-flex">Jozaguts</h1>
     </router-link>
     <v-spacer></v-spacer>
-    <v-list   dense class="d-none d-lg-flex d-md-flex justify-center align-center" >
-      <v-list-item v-for="link in links":key="link.to" class=" font-weight-bold" exact :to="link.to"  ripple>
-        <v-list-item-content class="text-capitalize" v-text="link.value"/>
+    <v-list
+      dense
+      class="d-none d-lg-flex d-md-flex justify-center align-center"
+    >
+      <v-list-item
+        v-for="link in links"
+        :key="link.to"
+        class=" font-weight-bold"
+        exact
+        :to="link.to"
+        ripple
+      >
+        <v-list-item-content class="text-capitalize" v-text="link.value" />
       </v-list-item>
-      <v-list-item class="accent--text text-center font-weight-bold" exact @click="turnOn" ripple>
+      <v-list-item
+        class="accent--text text-center font-weight-bold"
+        exact
+        @click="turnOn"
+        ripple
+      >
         <v-list-item-content>
           <v-btn>
             <v-icon color="primary">far fa-lightbulb</v-icon>
@@ -21,33 +36,57 @@
 </template>
 
 <script>
-import {mapMutations} from 'vuex';
+import { mapMutations } from "vuex";
 export default {
   data() {
     return {
-      links: [
-        {
-          to: 'about-me',
-          value: 'About Me'
-        },
-        {
-          to: 'blog',
-          value: 'Blog'
-        },
-        {
-          to: 'resume',
-          value: 'resume'
-        }
-      ]
+      linksData: {
+        en: [
+          {
+            to: "about-me",
+            value: "About Me"
+          },
+          {
+            to: "blog",
+            value: "Blog"
+          },
+          {
+            to: "resume",
+            value: "resume"
+          }
+        ],
+        es: [
+          {
+            to: "/es/about-me",
+            value: "Acerca de"
+          },
+          {
+            to: "/es/blog",
+            value: "Blog"
+          },
+          {
+            to: "/es/resume",
+            value: "resume"
+          }
+        ]
+      }
+    };
+  },
+  computed: {
+    links() {
+      return this.$i18n.locale === "en" ? this.linksData.en : this.linksData.es;
+    },
+    homeRoute() {
+      return this.$i18n.locale === "en" ? "/" : "/es";
     }
   },
   methods: {
     ...mapMutations({
-      changeDrawerStatus: 'global/toggleDrawerStatus'
+      changeDrawerStatus: "global/toggleDrawerStatus"
     }),
     turnOn() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-    },
+    }
   }
 };
 </script>
@@ -56,11 +95,9 @@ export default {
 
 a
   text-decoration: none
-  
+
 .v-list-item__content
   min-width: 100px
   text-align: center
   display: inline
-
-
 </style>
